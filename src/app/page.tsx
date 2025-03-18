087d32652f3a6342f49c3e7230d5f0aef3396728
manassaharoy <roymanassaha@gmail.com>
2025-03-19 01:31:23 +0600

    feat: added env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div className="text-center">
       <p>Hello from nextjs</p>
+      <p>PUBLIC SECRET: {process.env.NEXT_PUBLIC_SECRET}</p>
+      <p>SECRET: {process.env.SECRET_KEY}</p>
     </div>
   );
 }
